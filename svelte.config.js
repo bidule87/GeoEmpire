835aba8bb5fb9d09ffa9fda1a61b94ac3ddb8627
bidule87 <clavetmatt@gmail.com>
2026-02-08 21:49:47 +0100

    Change adapter to auto and simplify config

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,10 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-		experimental: {
-			remoteFunctions: true
-		}
-	},
-	compilerOptions: {
-		experimental: {
-			async: true
-		}
-	}
+    kit: {
+        adapter: adapter()
+    }
 };
 
 export default config;
