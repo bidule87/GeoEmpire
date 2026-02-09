34d67adaedc34bacb95b52f52eb34d5dd0ddf103
bidule87 <clavetmatt@gmail.com>
2026-02-09 21:04:42 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,20 @@
-// On va créer des variables pour chaque cadenas
-let progress1 = 0;
-let progress2 = 0;
-let progress3 = 0;
+// On initialise les niveaux des 3 cadenas
+let lock1 = 0; let lock2 = 0; let lock3 = 0;
+let heistActive = false;
 
-function hackLock(id) {
-    // Chaque clic fait avancer la barre de 10%
-    if (id === 1) progress1 += 10;
-    if (id === 2) progress2 += 10;
-    if (id === 3) progress3 += 10;
-    
-    // Si la barre atteint 100%, le cadenas est forcé !
+function startHeist() {
+    heistActive = true;
+    // Boucle qui fait redescendre les jauges (la difficulté)
+    const decay = setInterval(() => {
+        if (!heistActive) clearInterval(decay);
+        if (lock1 > 0) lock1 -= 1.5;
+        if (lock2 > 0) lock2 -= 1.5;
+        if (lock3 > 0) lock3 -= 1.5;
+    }, 1000);
+}
+
+function tapLock(id) {
+    if (id === 1 && lock1 < 100) lock1 += 8;
+    if (id === 2 && lock2 < 100) lock2 += 8;
+    if (id === 3 && lock3 < 100) lock3 += 8;
 }
